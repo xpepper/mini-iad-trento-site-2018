9872b82bcf2445b06b1e60070037941b16b56a36
Pietro Di Bello <pierodibello@gmail.com>
2017-11-13 18:02:42 +0100

    moves countdown images into img folder

--- js/countdown/countdown.js
+++ js/countdown/countdown.js
@@ -14,7 +14,7 @@
 ////////////////////////////////////////////
 
 
-var CountdownImageFolder = "./js/countdown";
+var CountdownImageFolder = "img/";
 var CountdownImageBasename = "flipper";
 var CountdownImageExt = "png";
 var CountdownImagePhysicalWidth = 41;
